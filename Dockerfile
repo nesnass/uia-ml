851ea7806e60ca82ae998546219d87a4cef370e6
Dimitris Panouris <dimitris@grm-studby-128-39-148-206.studby.uia.no>
2020-02-05 15:07:10 +0100

    changed app to run on port 80

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,4 @@ RUN pip install -r requirements.txt
 COPY ./src/ /app/
 ENV FLASK_APP=app.py
 
-CMD flask run --host=0.0.0.0 --port=80
+CMD flask run
--- src/app.py
+++ src/app.py
@@ -404,4 +404,4 @@ def result_string(result):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
